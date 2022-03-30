0564be8c9a6fb74d589755f07301551990dfd23c
Peter <piter.kyiv@gmail.com>
2022-03-30 11:49:05 -0700

    Add files via upload
    Dog age to human age formula.

---
+++ DogAgeToHumanAgeFormula.js
@@ -0,0 +1,8 @@
+// Get the dog's age through a prompt followed by saving.
+var dogAge = prompt("What is your dog age?");
+
+// Convert dog age to human age.
+var humanAge = (dogAge - 2) * 4 + 21;
+
+// Show the result to the user with a alert message.
+alert("The age of your dog to human age, " + humanAge);
