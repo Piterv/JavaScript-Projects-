b99b146514dbea730a364e1823d64058ac0c4951
Peter <piter.kyiv@gmail.com>
2022-03-30 13:48:54 -0700

    Update DogAgeToHumanAgeFormula.js
    Added precedence rule to humanAge formula, parentheses. 
    Edited alert text.

--- DogAgeToHumanAgeFormula.js
+++ DogAgeToHumanAgeFormula.js
@@ -1,8 +1,9 @@
 // Get the dog's age through a prompt followed by saving.
-var dogAge = prompt("What is your dog age?");
+var dogAge = prompt("How old is your dog?");
 
-// Convert dog age to human age.
-var humanAge = (dogAge - 2) * 4 + 21;
+// Convert dog age formula to human age.
+var humanAge = ((dogAge - 2) * 4) + 21;
 
 // Show the result to the user with a alert message.
-alert("The age of your dog to human age, " + humanAge);
+alert("Your dog is " + humanAge + " years old in human years.");
+
