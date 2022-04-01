78100802d5d0cdd52cabf40f6cda3853867c0284
Peter <piter.kyiv@gmail.com>
2022-04-01 11:33:25 -0700

    Update BMI Calculator.js
    Added function Math.pow() for correct calculation of BMI value.

--- BMI Calculator.js
+++ BMI Calculator.js
@@ -1,11 +1,11 @@
 //Function takes weight and height inputs and gives the calculated BMI value.
 function bmiCalculator(weight, height){
     //Store weight parameter.
-    var weight = prompt("what is your whait?");
+    var weight = prompt("what is your weight?");
     // Store heigh information.
     var height = prompt("What is your height?");
     //Calculate BMI value.
-    var bmi = Math.round(weight / (height * 2));
+    var bmi = Math.round(weight / (Math.pow(height, 2)));
     return bmi;
 }
 
