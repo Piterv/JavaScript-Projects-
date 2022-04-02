b70bca26819ca240b2dbdda7331e1ee91da05070
Peter <piter.kyiv@gmail.com>
2022-04-01 21:26:35 -0700

    Update BMI Calculator.js
    Added conditions that output different messages depending on the BMI.

--- BMI Calculator.js
+++ BMI Calculator.js
@@ -1,12 +1,22 @@
 //Function takes weight and height inputs and gives the calculated BMI value.
-function bmiCalculator(weight, height){
-    //Store weight parameter.
-    var weight = prompt("what is your weight?");
-    // Store heigh information.
-    var height = prompt("What is your height?");
-    //Calculate BMI value.
+
+function bmiCalculator(){ 
+    
+    var weight = prompt("what is your weight?"); //Store weight parameter.
+    var height = prompt("What is your height?"); // Store heigh information.
+    
     var bmi = Math.round(weight / (Math.pow(height, 2)));
-    return bmi;
+    var interpretation;
+     
+    if(bmi < 18.5){
+        interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+    }if(bmi >= 18.5 && bmi <= 24.9){
+        interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+    }if(bmi > 24.9){
+        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+    }
+    
+    return interpretation;
 }
 
-console.log(bmiCalculator(weight, height));
+alert(bmiCalculator());
