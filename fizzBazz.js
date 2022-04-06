905c2b0b718888c4e40a6fa30b291b7a3dab78a8
Peter <piter.kyiv@gmail.com>
2022-04-06 10:11:26 -0700

    Update fizzBazz.js
    Removed function output. pop(); which removes the last element in the output array.
    Added output. push(number); to the else condition, so that after all the conditions have been checked, the number will be placed in the array.

--- fizzBazz.js
+++ fizzBazz.js
@@ -2,19 +2,17 @@ var output = [];
 var count = 1;
 
 function fizzBuzz() {
-//Runs the code with the specified number.
-  while (count < 100) {
 
-    output.push(count);
+  while (count <= 100) {
+
     if (count % 3 === 0 && count % 5 === 0) {
-      output.pop();
       output.push("FizzBuzz");
     } else if (count % 3 === 0) {
-      output.pop();
       output.push("Fizz");
     } else if (count % 5 === 0) {
-      output.pop();
       output.push("Bazz");
+    } else {
+      output.push(count);
     }
 
     count++;
