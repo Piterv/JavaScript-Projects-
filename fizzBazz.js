c56aaa6d237844dd432a8425cd55715c378231b2
Peter <piter.kyiv@gmail.com>
2022-04-06 10:00:36 -0700

    Update fizzBazz.js
    Added a while loop to output a sequence of the specified number.

--- fizzBazz.js
+++ fizzBazz.js
@@ -2,20 +2,24 @@ var output = [];
 var count = 1;
 
 function fizzBuzz() {
+//Runs the code with the specified number.
+  while (count < 100) {
 
-  output.push(count);
-  
-  if (count % 3 === 0 && count % 5 === 0) {
-    output.push("FizzBuzz");
-  } else if (count % 3 === 0) {
-    output.push("Fizz");
-  } else if (count % 5 === 0) {
-    output.push("Bazz");
-  } else if (count === 100) {
+    output.push(count);
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.pop();
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      output.pop();
+      output.push("Fizz");
+    } else if (count % 5 === 0) {
+      output.pop();
+      output.push("Bazz");
+    }
+
+    count++;
     console.log(output);
-  } 
-  count++;
-  console.log(output);
+  }
 }
-// Calling function.
+
 fizzBuzz();
