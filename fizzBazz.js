969e69d4a286f23e283820ea3097ac0971f3e041
Peter <piter.kyiv@gmail.com>
2022-04-05 22:08:34 -0700

    Update fizzBazz.js
    Changing the control flow of the if statement to be more readable.

--- fizzBazz.js
+++ fizzBazz.js
@@ -4,24 +4,18 @@ var count = 1;
 function fizzBuzz() {
 
   output.push(count);
-
-  if (count % 3 === 0) {
-    if (count % 3 === 0 && count % 5 === 0) {
-      count++;
-      output.pop();
-      output.push("fizzBuzz");
-    } else {
-      count++;
-      output.pop();
-      output.push("fizz");
-    }
+  
+  if (count % 3 === 0 && count % 5 === 0) {
+    output.push("FizzBuzz");
+  } else if (count % 3 === 0) {
+    output.push("Fizz");
   } else if (count % 5 === 0) {
-    count++;
-    output.pop();
     output.push("Bazz");
-  } else {
-    count++;
-  }
+  } else if (count === 100) {
+    console.log(output);
+  } 
+  count++;
+  console.log(output);
 }
-
+// Calling function.
 fizzBuzz();
