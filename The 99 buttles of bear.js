7a996187b2efff972ae56bf4d9911175553264f8
Peter <piter.kyiv@gmail.com>
2022-04-06 21:55:52 -0700

    Update The 99 buttles of bear.js
    Added function buttlesOfBear(). 
    Now the code is run by calling the function.

--- The 99 buttles of bear.js
+++ The 99 buttles of bear.js
@@ -1,18 +1,22 @@
 var NumberOfButtles = 10;
 
-while (NumberOfButtles >= 0) {
+function buttlesOfBear(){
+while(NumberOfButtles >= 0){
 
-  var bouttleWord = "bottles";
-  var NumberOfButtlesLeft = NumberOfButtles - 1;
+    var NumberOfButtlesLeft = NumberOfButtles - 1; 
 
-  if (NumberOfButtles > 1) {
-    console.log(NumberOfButtles + " bottles of beer on the wall, " + NumberOfButtles + " bottles of beer. Take one down and pass it around, " + NumberOfButtlesLeft + " buttles of beer on the wall.");
-  } else if (NumberOfButtles === 1) {
+    if(NumberOfButtles > 2){
+     console.log(NumberOfButtles + " bottles of beer on the wall, " + NumberOfButtles + " bottles of beer. Take one down and pass it around, " + NumberOfButtlesLeft + " buttles of beer on the wall.");
+    }else if(NumberOfButtles === 2){
     console.log(NumberOfButtles + " bottles of beer on the wall, " + NumberOfButtles + " bottles of beer. Take one down and pass it around, " + NumberOfButtlesLeft + " buttle of beer on the wall.");
-  } else if (NumberOfButtles === 0) {
-    bouttleWord = "bottle";
-    console.log("No more bottle of beer on the wall!");
-  }
-
-  NumberOfButtles--;
+    }else if(NumberOfButtles === 1){
+       console.log(NumberOfButtles + " bottles of beer on the wall, " + NumberOfButtles + " bottles of beer. Take one down and pass it around, " + NumberOfButtlesLeft + " buttle of beer on the wall.");
+    }else if(NumberOfButtles === 0){
+        bouttleWord = "bottle";
+        console.log("No more bottle of beer on the wall!");
+    }
+    
+    NumberOfButtles--;
 }
+}
+buttlesOfBear();
